e8e3c67fdfa22e6f52199413a648dc6d0b0db917
abhilash1910 <debabhi1396@gmail.com>
2020-09-14 11:33:08 +0530

    Modified Release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
   author = 'ABHILASH MAJUMDER',
   author_email = 'debabhi1396@gmail.com',
   url = 'https://github.com/abhilash1910/NexGCN',   
-  download_url = 'https://github.com/abhilash1910/../',    
+  download_url = 'https://github.com/abhilash1910/NexGCN/archive/v_0.1.tar.gz/',    
   keywords = ['graph_convolution_network', 'binary classification', 'GCN','graph neural network','Spectral GCN','Networkx GCN','centrality GCN','keras GCN','sigmoid Graph convolution network'],   
   install_requires=[           
 
