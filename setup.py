59086365cfba0a49b5a491aeb5f252533da8f6e7
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2020-11-19 22:02:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ from distutils.core import setup
 setup(
   name = 'NexGCN',         
   packages = ['NexGCN'],   
-  version = '0.1',       
+  version = '0.2',       
   license='MIT',        
   description = 'A Spectral Sigmoid Graph Convolution Network Library for binary classification on Networkx Graphs compatible with Keras and Tensorflow',   
   author = 'ABHILASH MAJUMDER',
   author_email = 'debabhi1396@gmail.com',
   url = 'https://github.com/abhilash1910/NexGCN',   
-  download_url = 'https://github.com/abhilash1910/NexGCN/archive/v_0.1.tar.gz/',    
+  download_url = 'https://github.com/abhilash1910/NexGCN/archive/v_0.2.tar.gz/',    
   keywords = ['graph_convolution_network', 'binary classification', 'GCN','graph neural network','Spectral GCN','Networkx GCN','centrality GCN','keras GCN','sigmoid Graph convolution network'],   
   install_requires=[           
 
